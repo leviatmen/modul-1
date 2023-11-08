b9a735a9bcd0f63962eb8c452fa58ab0e21119e1
D:\KULIAH\SEMESTER 5\PRAKTIKUM IPL <firmannur21@if.unjani.ac.id>
2023-11-08 11:32:56 +0700

    exceptest2/errorhandling

---
+++ modul5/LatihanDefensiveProgramming/ExcepTest2.java
@@ -0,0 +1,19 @@
+package LatihanDefensiveProgramming;
+
+import java.io.*;
+
+public class ExcepTest2 {
+    public static void main(String[] args) {
+        try {
+            file = new FileInputStream(fileName);
+            x = (byte) file.read();
+        } catch (IOException i) {
+            i.printStackTrace();
+            return -1;
+        } catch (FileNotFoundException f) // Not valid! {
+            f.printStackTrace();
+            return -1;
+            
+    }
+
+}
